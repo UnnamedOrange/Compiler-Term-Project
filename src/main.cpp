295b5fd40f9abc0f8d6c14f7ece812366da9bb49
UnnamedOrange <lycheng1215@sina.com>
2022-11-14 19:43:00 +0800

    Traverse Koopa to generate RISC-V

--- src/main.cpp
+++ src/main.cpp
@@ -72,6 +72,140 @@ void traverse_koopa_first_example(const std::string& str)
     koopa_delete_raw_program_builder(builder);
 }
 
+// 函数前向声明。
+std::string Visit(const std::string&);
+std::string Visit(const koopa_raw_program_t&);
+std::string Visit(const koopa_raw_slice_t&);
+std::string Visit(const koopa_raw_function_t&);
+std::string Visit(const koopa_raw_basic_block_t&);
+std::string Visit(const koopa_raw_value_t&);
+std::string Visit(const koopa_raw_return_t&);
+
+// 用于处理整个 raw program 的函数
+std::string Visit(const std::string& str)
+{
+    // 解析字符串 str, 得到 Koopa IR 程序
+    koopa_program_t program;
+    koopa_error_code_t ret = koopa_parse_from_string(str.c_str(), &program);
+    assert(ret == KOOPA_EC_SUCCESS); // 确保解析时没有出错
+    // 创建一个 raw program builder, 用来构建 raw program
+    koopa_raw_program_builder_t builder = koopa_new_raw_program_builder();
+    // 将 Koopa IR 程序转换为 raw program
+    koopa_raw_program_t raw = koopa_build_raw_program(builder, program);
+    // 释放 Koopa IR 程序占用的内存
+    koopa_delete_program(program);
+
+    auto ret_riscv = Visit(raw);
+
+    // 处理完成, 释放 raw program builder 占用的内存
+    // 注意, raw program 中所有的指针指向的内存均为 raw program builder
+    // 的内存 所以不要在 raw program 处理完毕之前释放 builder
+    koopa_delete_raw_program_builder(builder);
+
+    return ret_riscv;
+}
+
+// 访问 raw program
+std::string Visit(const koopa_raw_program_t& program)
+{
+    // 执行一些其他的必要操作
+    std::string ret;
+    // 访问所有全局变量
+    ret += "    .text\n";
+    ret += Visit(program.values);
+    for (size_t i = 0; i < program.funcs.len; ++i)
+    {
+        ret += std::string("    .globl ") +
+               (reinterpret_cast<koopa_raw_function_t>(program.funcs.buffer[i])
+                    ->name +
+                1) +
+               "\n";
+    }
+    // 访问所有函数
+    ret += Visit(program.funcs);
+    return ret;
+}
+
+// 访问 raw slice
+std::string Visit(const koopa_raw_slice_t& slice)
+{
+    std::string ret;
+    for (size_t i = 0; i < slice.len; ++i)
+    {
+        auto ptr = slice.buffer[i];
+        // 根据 slice 的 kind 决定将 ptr 视作何种元素
+        switch (slice.kind)
+        {
+        case KOOPA_RSIK_FUNCTION:
+            // 访问函数
+            ret += Visit(reinterpret_cast<koopa_raw_function_t>(ptr));
+            break;
+        case KOOPA_RSIK_BASIC_BLOCK:
+            // 访问基本块
+            ret += Visit(reinterpret_cast<koopa_raw_basic_block_t>(ptr));
+            break;
+        case KOOPA_RSIK_VALUE:
+            // 访问指令
+            ret += Visit(reinterpret_cast<koopa_raw_value_t>(ptr));
+            break;
+        default:
+            // 我们暂时不会遇到其他内容, 于是不对其做任何处理
+            assert(false);
+        }
+    }
+    return ret;
+}
+
+// 访问函数
+std::string Visit(const koopa_raw_function_t& func)
+{
+    // 执行一些其他的必要操作
+    // ...
+    // 访问所有基本块
+    return std::string(func->name + 1) + ":\n" + Visit(func->bbs);
+}
+
+// 访问基本块
+std::string Visit(const koopa_raw_basic_block_t& bb)
+{
+    // 执行一些其他的必要操作
+    // ...
+    // 访问所有指令
+    return Visit(bb->insts);
+}
+
+// 访问指令
+std::string Visit(const koopa_raw_value_t& value)
+{
+    std::string ret;
+    // 根据指令类型判断后续需要如何访问
+    const auto& kind = value->kind;
+    switch (kind.tag)
+    {
+    case KOOPA_RVT_RETURN:
+        // 访问 return 指令
+        ret += Visit(kind.data.ret);
+        break;
+    case KOOPA_RVT_INTEGER:
+        // 访问 integer 指令
+        // ret += Visit(kind.data.integer);
+        break;
+    default:
+        // 其他类型暂时遇不到
+        assert(false);
+    }
+    return ret;
+}
+
+// 访问对应类型指令的函数定义略
+// 视需求自行实现
+std::string Visit(const koopa_raw_return_t& return_inst)
+{
+    return "    li a0, " +
+           std::to_string(return_inst.value->kind.data.integer.value) + "\n" +
+           "    ret\n";
+}
+
 int main(int argc, const char* argv[])
 {
     // 解析命令行参数. 测试脚本/评测平台要求你的编译器能接收如下参数:
@@ -107,6 +241,8 @@ int main(int argc, const char* argv[])
     else if (mode[1] == 'r')
     {
         // traverse_koopa_first_example(code);
+        ofstream ofs(output);
+        ofs << Visit(code);
     }
 
     return 0;
